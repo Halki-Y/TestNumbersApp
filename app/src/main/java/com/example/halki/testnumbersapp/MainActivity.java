b422061d3bac2d0f73e579b1443afc3d673915c7
Halki-Y <halki@ii.net>
2017-12-17 16:56:15 +0800

    Started

--- app/src/main/java/com/example/halki/testnumbersapp/MainActivity.java
+++ app/src/main/java/com/example/halki/testnumbersapp/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.halki.testnumbersapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.*;
+
 
 public class MainActivity extends AppCompatActivity {
+    Random rand = new Random();
+    private int upLimit;
+    private int lowLimit;
+    private int guessNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start(500);
+    }
+    void start(int limit){
+        lowLimit = 0;
+        upLimit = limit;
+        guess();
+    }
+    void guess(){
+        guessNumber = rand.nextInt(upLimit+1-lowLimit)+lowLimit;
+
+    }
+    void lower(){
+        upLimit = guessNumber;
+        guess();
+    }
+    void higher(){
+        lowLimit = guessNumber;
+        guess();
+    }
+    void correct(){
+
     }
 }
